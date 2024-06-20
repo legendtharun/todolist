d7d17bb70bfd483e0ad2e37c0d58a92358f09897
legendtharun <manikandan211205@gmail.com>
2024-06-20 21:18:44 +0530

    upcoming deadlines commit

--- src/Calender.js
+++ src/Calender.js
@@ -31,25 +31,85 @@ export default function Calender() {
       //   console.log(selected_date);
       let todo1 = document.getElementById("todolist1");
       let todo2 = document.getElementById("todolist2");
-      let uptodo1 = document.getElementById("uptodolist1");
-      let uptodo2 = document.getElementById("uptodolist2");
       if (curr_id === selected_date) {
         todo1.innerText = tododata[i].task1;
         todo2.innerText = tododata[i].task2;
-        try {
-          uptodo1.innerText = tododata[i + 1].task1;
-          uptodo2.innerText = tododata[i + 1].task2;
-        } catch (e) {
-          uptodo1.innerText = "Looks like there is no upcoming tasks.";
-          uptodo2.innerText = "Looks like there is no upcoming tasks.";
-        }
+        // try {
+        //   uptodo1.innerText = tododata[i + 1].task1;
+        //   uptodo2.innerText = tododata[i + 1].task2;
+        // } catch (e) {
+        //   uptodo1.innerText = "Looks like there is no upcoming tasks.";
+        //   uptodo2.innerText = "Looks like there is no upcoming tasks.";
+        // }
         break;
       } else {
         todo1.innerText = "Today is free!!";
         todo2.innerText = "No tasks there";
-        uptodo1.innerText = "Looks like there is no upcoming tasks.";
-        uptodo2.innerText = "Looks like there is no upcoming tasks.";
+
+        // uptodo1.innerText = "Looks like there is no upcoming tasks.";
+        // uptodo2.innerText = "Looks like there is no upcoming tasks.";
       }
+    }
+    let uptodo1 = document.getElementById("uptodolist1");
+    let uptodo2 = document.getElementById("uptodolist2");
+    let todays_date = today[2];
+    let month_lessdays = ["Feb", "Apr", "Jun", "Sept", "Nov"];
+    let month_moredays = ["Jan", "Mar", "May", "July", "Aug", "Oct", "Dec"];
+    var now = new Date();
+    var nextMonth;
+
+    if (now.getMonth() === 11) {
+      // If it's December, set to January of the following year
+      nextMonth = new Date(now.getFullYear() + 1, 0, 1);
+    } else {
+      // Otherwise, set to the first day of the next month
+      nextMonth = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+    }
+    nextMonth = String(nextMonth).split(" ");
+
+    if (
+      (todays_date < 30 && month_lessdays.includes(today[1])) ||
+      (todays_date < 31 && month_moredays.includes(today[1]))
+    ) {
+      // console.log("yes");
+
+      for (let j = 0; j < tododata.length; j++) {
+        let curr_date = String(tododata[j].id).split(" ");
+        // console.log(curr_date);
+        if (
+          curr_date[0] > today[2] &&
+          curr_date[1] === today[1] &&
+          curr_date[2] === today[3]
+        ) {
+          // console.log("success");
+          // console.log(tododata[j]);
+          uptodo1.innerText = tododata[j].task1;
+          uptodo2.innerText = tododata[j].task2;
+          break;
+        }
+      }
+    } else if (
+      (todays_date == 30 && month_lessdays.includes(today[1])) ||
+      (todays_date == 31 && month_moredays.includes(today[1]))
+    ) {
+      for (let j = 0; j < tododata.length; j++) {
+        let curr_date = String(tododata[j].id).split(" ");
+        // console.log("yes");
+        if (
+          curr_date[0] < today[2] &&
+          curr_date[1] === nextMonth[1] &&
+          curr_date[2] === today[3]
+        ) {
+          uptodo1.innerText = tododata[j].task1;
+          uptodo2.innerText = tododata[j].task2;
+          break;
+        }
+      }
+    }
+    if (uptodo1.innerText === "") {
+      // console.log("yes");
+      uptodo1.innerText = "Looks like there is no upcoming tasks.";
+      uptodo2.innerText = "Looks like there is no upcoming tasks.";
     }
   }
   useEffect(() => {
